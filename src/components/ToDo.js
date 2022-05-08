b227d5fede0c7aeb0d492311190763b423e4cb0d
aaramiss <31129748+aaramiss@users.noreply.github.com>
2022-05-08 16:24:20 +0430

    change todos name and classNames of buttons

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,25 +1,26 @@
 import React from 'react';
 
-
-const ToDo = ({ index, todos, handleToggle, removeTask }) => {
+const ToDo = ({ todo, handleToggle, handleRemove }) => {
 
     return (
-        <div
-            id={todos.id}
-            value={todos.id}
-            key={todos.id + todos.text}
-            className={todos.status === "in progress" ? "todos" : "todos strike"}
-            name="todo"
-        >
-            {todos.text}
-            <div>
-            <button className="task" onClick={() => handleToggle(index)}>
-                {todos.status === "inprogress" ? <i className="far fa-square"></i>
-                    : <i className="far fa-check-square"></i>}
-            </button>
-            <button className="task" onClick={() => removeTask(index)}>
-                <i className="fa fa-xmark"></i>
-            </button>
+        <div id={todo.id}
+            className={todo.status === "in progress" ? "todo" : "todo strike"} >
+            {todo.text}
+            <div className="btn-wrapper">
+                <button className="btn" onClick={() => handleToggle(todo.id)}>
+                    {todo.status === "in progress" ?
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={1}>
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
+                        </svg>
+                        : <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                            <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" />
+                        </svg>}
+                </button>
+                <button className="btn" onClick={() => handleRemove(todo.id)}>
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={1}>
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+                    </svg>
+                </button>
             </div>
         </div>
 
