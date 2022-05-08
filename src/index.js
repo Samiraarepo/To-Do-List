ae9da49e251d3f83a7ba9de35058d4c8e0e2a4c9
aaramiss <31129748+aaramiss@users.noreply.github.com>
2022-05-08 16:28:45 +0430

    import style.css in index

--- src/index.js
+++ src/index.js
@@ -2,4 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+import './style.css';
+
+
 ReactDOM.createRoot(document.querySelector('#root')).render(<App />);
