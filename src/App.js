c4b69fc0f7c17877ac4b0db9793d9799af732939
aaramiss <31129748+aaramiss@users.noreply.github.com>
2022-05-08 16:26:20 +0430

    move inside of App to ToDoPage

--- src/App.js
+++ src/App.js
@@ -1,50 +1,12 @@
-import React, { useState } from 'react';
+import React from 'react';
+import ToDoPage from './ToDoPage';
 import Header from './components/Header';
-import data from './data.json';
-import ToDoList from './components/ToDoList';
-import ToDoForm from './components/ToDoForm';
-
-import './App.css';
 
 const App = () => {
-    const [toDoList, setToDoList] = useState(data);
-
-    const handleToggle = index => {
-        const mapped = toDoList.map(task => {
-            return (
-                task.id === Number(index + 1) && task.status === "in progress" ? { ...task, status: "done" }
-                    : { ...task, status: "in progress" } //!!
-            )
-        });
-        setToDoList(mapped);
-    }
-
-    const removeTask = index => {
-        console.log(index);
-        const newTasks = [...toDoList];
-        newTasks.splice(index, 1);
-        setToDoList(newTasks);
-    }
-
-    const addTask = (userInput) => {
-        let newTask = [...toDoList];
-        newTask = [...newTask, {
-            id: toDoList.length + 1, text: userInput,
-            status: "in progress"
-        }];
-        setToDoList(newTask);
-    };
-
     return (
         <div className="App">
             <Header />
-            <div className="container">
-                <ToDoList toDoList={toDoList}
-                    handleToggle={handleToggle}
-                    removeTask={removeTask}
-                />
-                <ToDoForm addTask={addTask} />
-            </div>
+            <ToDoPage />
         </div>
     )
 }
